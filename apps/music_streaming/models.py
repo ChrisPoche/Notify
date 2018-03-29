2f203a6e91ac0132d7dc887bc688f1fdc18ebdb7
Ryan Magley <ryan.magley@gmail.com>
2018-03-29 12:24:22 -0500

    Added readme.md

--- apps/music_streaming/models.py
+++ apps/music_streaming/models.py
@@ -4,33 +4,6 @@ from __future__ import unicode_literals
 from django.db import models
 from ..login.models import User
 
-class Track(models.Model):
-    title = models.CharField(max_length=255)
-    tracknumber = models.IntegerField()
-    length = models.IntegerField() # length in seconds
-    location = models.FileField(upload_to='music/')
-    interests = models.ManyToManyField(Interest, related_name='tracks')
-    followers = models.ManyToManyField(User, related_name='follows')
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-class Artist(models.Model):
-    name = models.CharField(max_length=255)
-    desc = models.TextField()
-    followers = models.ManyToManyField(User, related_name='follows')
-    artist_image = models.ImageField(upload_to='artistimages/')
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-class Album(models.Model):
-    name = models.CharField(max_length=255)
-    releaseyear = models.IntegerField(max_length=4)
-    artists = models.ManyToManyField(Artist, related_name='artists')
-    interests = models.ManyToManyField(InterestGenre, related_name='albums')
-    album_image = models.ImageField(upload_to='albumimages/')
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
 class Interest(models.Model):
     name = models.CharField(max_length=255)
     type = models.CharField(max_length=1) # 'i' for instrument, 'g' for genre
@@ -45,6 +18,33 @@ class InterestInstrument(models.Model):
 
 class InterestGenre(models.Model):
     interest = models.OneToOneField(Interest, on_delete=models.CASCADE, primary_key=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Track(models.Model):
+    title = models.CharField(max_length=255)
+    tracknumber = models.IntegerField()
+    length = models.IntegerField() # length in seconds
+    location = models.FileField(upload_to='music/')
+    interests = models.ManyToManyField(Interest, related_name='tracks')
+    followers = models.ManyToManyField(User, related_name='follows_track')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Artist(models.Model):
+    name = models.CharField(max_length=255)
+    desc = models.TextField()
+    followers = models.ManyToManyField(User, related_name='follows_artist')
+    artist_image = models.ImageField(upload_to='artistimages/', default='defaultartist.jpg')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Album(models.Model):
+    name = models.CharField(max_length=255)
+    releaseyear = models.IntegerField(max_length=4)
+    artists = models.ManyToManyField(Artist, related_name='artists')
+    interests = models.ManyToManyField(InterestGenre, related_name='albums')
+    album_image = models.ImageField(upload_to='albumimages/')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
